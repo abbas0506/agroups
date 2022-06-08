6719a819d8255dce01975c88ea98607b34e710d1
mianazeemdaula <mianazeemdaula@gmail.com>
2022-06-08 21:18:01 +0500

    course

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -18,7 +18,9 @@ class CourseSeeder extends Seeder
         Course::create(['name' => 'Mobile Apps']);
         Course::create(['name' => 'Wordpress']);
         Course::create(['name' => 'Web Development']);
-        Course::create(['name' => 'Amazon']);
+        Course::create(['name' => 'Amazon Virtual Assistant']);
+        Course::create(['name' => 'Amazon FBA Wholesale']);
+        Course::create(['name' => 'Amazon FBA Private Label']);
         Course::create(['name' => 'Graphics']);
         Course::create(['name' => 'GK Preparation']);
     }
