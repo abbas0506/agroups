66510eedc8a79cb58e51dd63bc00d32dca0373b2
mianazeemdaula <mianazeemdaula@gmail.com>
2022-06-17 20:13:58 +0500

    orientation

--- resources/views/students/success.blade.php
+++ resources/views/students/success.blade.php
@@ -11,6 +11,11 @@
         <h1>Congratualtion!</h1>
         <p class="mt-2">You have successfully registered on schooloftechnologies.com. Keep in touch, soon you will
             be informed via email or phone about the classes.</p>
+
+        <div class="txt-grey txt-l txt-b lh-40">Plese Attened the Orientation June 19, 2022 05:00 PM</div>
+        <div class="txt-grey txt-l txt-b lh-40">Venue: <a href="https://www.google.com/maps/@30.6722083,73.6587437,21z"
+                target="_blannk"> Rasheed-u-Din Colony, Street # 1, Near Wagon Stand, Depalpur</a></div>
+
         <a href="/" class="txt-blue mt-3" style="float:right">
             <button class="btn btn-success">Ok, I'll</button>
         </a>
